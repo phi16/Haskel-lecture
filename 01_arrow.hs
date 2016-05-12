e7b3fdde2833b03171eeba163bf898c904deef8b
phi16 <phi16.256@gmail.com>
2016-05-12 22:56:55 +0900

    Add : Arrow

---
+++ 01_arrow.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE Arrows #-}
+
+import qualified Data.Stream as S
+import Control.Arrow
+import Control.Arrow.Operations
+import Control.Arrow.Transformer.Stream
+
+sumStream :: StreamMap Int Int
+sumStream = proc x -> do
+  rec
+    a <- returnA -< x + s
+    s <- delay 0 -< a
+  returnA -< a
+
+fibStream :: StreamMap () Int
+fibStream = proc () -> do
+  rec
+    x <- delay 1 -< y
+    y <- delay 1 -< x+y
+  returnA -< x
+
+sums :: S.Stream Int -> S.Stream Int
+sums xs = runStream (arr snd >>> sumStream) ((),xs)
+
+fibs :: S.Stream Int
+fibs = runStream (arr snd >>> fibStream) ((),S.repeat ())
+
