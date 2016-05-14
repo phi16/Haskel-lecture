fcd8a7996d44db0dccc75b8621c2c4839b040872
phi16 <phi16.256@gmail.com>
2016-05-14 22:25:19 +0900

    Add 04 : Easiest impl

--- 04_regex.hs
+++ 04_regex.hs
@@ -1,2 +1,19 @@
--- Matching
+import Data.List
 
+data Reg = Chr Char
+         | Seq Reg Reg
+         | Alt Reg Reg
+         | Rep Reg
+         | Eps
+
+parts :: [a] -> [([a],[a])]
+parts s = zip (inits s) (tails s)
+
+match :: Reg -> String -> Bool
+match (Chr c) s = [c] == s
+match (Seq l r) s = any (\(x,y) -> match l x && match r y) $ parts s
+match (Alt l r) s = match l s || match r s
+match (Rep e) s = any (all $ match e) $ cuts s where
+  cuts [] = [[]]
+  cuts xs = tail (parts xs) >>= \(as,bs) -> (as:) <$> cuts bs
+match Eps s = null s
