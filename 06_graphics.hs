d132f28c190f99697500d96dced27db672139e74
phi16 <phi16.256@gmail.com>
2016-05-23 00:18:55 +0900

    Impl : lifegame...
    very slow :P

--- 06_graphics.hs
+++ 06_graphics.hs
@@ -1,23 +1,55 @@
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import Data.Array
+import Control.Monad
+import Control.Lens hiding (rewrite)
+import System.Random
+
+w :: Int
+w = 400
 
 main :: IO ()
-main = play (InWindow "po" (400,400) (400,400)) black 60 iniWorld render handle step
+main = play (InWindow "po" (w,w) (w,w)) (greyN 0.5) 60 iniWorld render handle step
 
-newtype State = State (Bool,Float)
+type State = Array (Int,Int) Bool
+
+n :: Int
+n = 15
+
+sRange :: ((Int,Int),(Int,Int))
+sRange = ((0,0),(n-1,n-1))
 
 iniWorld :: State
-iniWorld = State (True,-100)
+iniWorld = listArray sRange $ cycle [False,True,True,True]
 
 render :: State -> Picture
-render (State (_,d)) = pictures [
-  color red $ rectangleSolid 60.0 60.0,
-  color yellow $ translate d 0 $ circleSolid 20.0 ]
+render a = pictures $ assocs a <&> \((x',y'),b) -> let
+    x = fromIntegral x'
+    y = fromIntegral y'
+    s = fromIntegral w / fromIntegral n
+    w' = fromIntegral w
+    t = translate (x*s-w'/2+s/2) (y*s-w'/2+s/2)
+    c = color $ if b then white else black
+    p = rectangleSolid (s-1) (s-1)
+  in t $ c p
+
+ini :: (Float,Float) -> State -> State
+ini (x',y') _ = listArray sRange $ randoms g where
+  g = mkStdGen $ floor $ x'*y'
 
 handle :: Event -> State -> State
-handle (EventKey (MouseButton LeftButton) Down _ _) (State (e,d)) = State (not e,d)
-handle _ s = s
+handle (EventKey (MouseButton LeftButton) Up _ p) = ini p
+handle _ = id
 
 step :: Float -> State -> State
-step f (State (e,d)) = State $ (e,d + f * e' * 30.0) where
-  e' = if e then 1 else -1
+step f a = listArray sRange $ assocs a <&> \((x,y),b) -> let
+    neighbors = do
+      i <- [x-1..x+1]
+      j <- [y-1..y+1]
+      guard $ i /= x || j /= y
+      guard $ inRange sRange (i,j)
+      guard $ a ! (i,j)
+    s = length neighbors
+  in if b
+    then s == 2 || s == 3
+    else s == 3
